855f2b07907447243813d169872e26dabd82b75b
Hassaan Akbar <hassaan.akbar@outlook.com>
2024-11-14 23:08:22 +0500

    changed to hash routing

--- wasm/src/router/index.ts
+++ wasm/src/router/index.ts
@@ -1,5 +1,5 @@
 // Composables
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } from 'vue-router';
 import { useNotebookStore } from '@/store/notebook';
 
 let notebookStore: any = null;
@@ -10,7 +10,7 @@ const routes = [
     component: () => import('@/views/HomePage.vue'),
   },
   {
-    path: '/lab',
+    path: '/app',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -26,12 +26,12 @@ const routes = [
 ] as RouteRecordRaw[];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
-});
+});``
 
 router.beforeEach((to, from) => {
-  if (!from.path.startsWith("/lab")) return;
+  if (!from.path.startsWith("/app")) return;
   if (!notebookStore) {
     notebookStore = useNotebookStore();
   }
